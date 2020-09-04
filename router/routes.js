247e6da9efce0e7df667ca40a3f2c77dee436392
joshuasearle <joshryansearle@gmail.com>
2020-09-04 14:29:06 +1000

    Add 404 and index route

--- router/routes.js
+++ router/routes.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
-const router = express.Router;
+const router = express.Router();
+
+router.get('/', (req, res) => res.render('index.ejs', { path: '/' }));
+
+router.use('/', (req, res) => res.render('404.ejs', { path: '/404' }));
 
 module.exports = router;

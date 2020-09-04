e94d0759ed6a45b1fb3a791493147cfee5c39e1b
joshuasearle <joshryansearle@gmail.com>
2020-09-04 21:35:41 +1000

    Add update and remove routes

--- router/router.js
+++ router/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const database = require('../models/database');
-
+const util = require('../util/util');
 const router = express.Router();
 
 router.get('/', (req, res) => res.render('index.ejs', { path: '/' }));
@@ -16,7 +16,6 @@ router.get('/add-book', (req, res) => {
 
 router.post('/add-book', async (req, res) => {
   const { title, isbn, created, summary, authorFirst, authorLast } = req.body;
-  console.log(created);
   try {
     await database.addBook(
       title,
@@ -39,11 +38,41 @@ router.get('/update-book', async (req, res) => {
 router.get('/update-book/:id', async (req, res) => {
   const id = req.params.id;
   try {
-    const book = database.findBookById(id);
-    res.render('single-book-update.ejs', { path: '/update-book', ...book });
+    const book = await database.findBookById(id);
+    res.render('single-book-update.ejs', {
+      path: '/update-book',
+      book: {
+        _id: book._id,
+        title: book.title,
+        author: book.author,
+        created: util.convertDate(book.created),
+      },
+    });
   } catch (e) {
     res.render('error.ejs', { path: '/error', message: e.message });
   }
+});
+
+router.post('/update-book/:id', async (req, res) => {
+  const id = req.params.id;
+  const { title, authorFirst, authorLast, created, summary } = req.body;
+  const author = { first: authorFirst, last: authorLast };
+  try {
+    await database.updateBook(id, title, author, created, summary);
+    res.redirect('/books');
+  } catch (e) {
+    res.render('error.ejs', { path: '/error', message: e.message });
+  }
+});
+
+router.get('/remove-book', (req, res) =>
+  res.render('remove-book.ejs', { path: '/remove-book' })
+);
+
+router.post('/remove-book', async (req, res) => {
+  const isbn = req.body.isbn;
+  await database.removeBookByIsbn(isbn);
+  res.redirect('/books');
 });
 
 router.use('/', (req, res) => res.render('404.ejs', { path: '/404' }));

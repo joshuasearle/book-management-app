d4aa9aed60685f44a7ee4daf7715363d76dd1052
joshuasearle <joshryansearle@gmail.com>
2020-09-04 16:24:30 +1000

    Add new routes

--- router/router.js
+++ router/router.js
@@ -1,20 +1,38 @@
 const express = require('express');
-
-const Book = require('../models/book.js');
-const Author = require('../models/author.js');
+const database = require('../models/database');
 
 const router = express.Router();
 
 router.get('/', (req, res) => res.render('index.ejs', { path: '/' }));
 
-router.get('/books', (req, res) => {
-  Book.find().then(books => {
-    res.render('books.ejs', { path: '/books', books: books });
-  });
+router.get('/books', async (req, res) => {
+  const books = await database.findBooks();
+  res.render('books.ejs', { path: '/books', books: books });
 });
 
 router.get('/add-book', (req, res) => {
   res.render('add-book.ejs', { path: '/add-book' });
+});
+
+router.post('/add-book', async (req, res) => {
+  const { title, isbn, created, summary, authorFirst, authorLast } = req.body;
+  try {
+    await database.addBook(title, {
+      first: authorFirst,
+      last: authorLast,
+      isbn,
+      created,
+      summary,
+    });
+    res.redirect('/books');
+  } catch (e) {
+    res.render('error.ejs', { path: '/error', message: e.message });
+  }
+});
+
+router.get('/update-book', async (req, res) => {
+  const books = await database.findBooks();
+  res.render('update-book.ejs', { path: '/update-book', books: books });
 });
 
 router.use('/', (req, res) => res.render('404.ejs', { path: '/404' }));

e61c341a1e5623b39a279f6bd6e7657a44e2d58d
joshuasearle <joshryansearle@gmail.com>
2020-09-04 16:36:56 +1000

    Add update book route

--- router/router.js
+++ router/router.js
@@ -35,6 +35,16 @@ router.get('/update-book', async (req, res) => {
   res.render('update-book.ejs', { path: '/update-book', books: books });
 });
 
+router.get('/update-book/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const book = database.findBookById(id);
+    res.render('single-book-update.ejs', { path: '/update-book', ...book });
+  } catch (e) {
+    res.render('error.ejs', { path: '/error', message: e.message });
+  }
+});
+
 router.use('/', (req, res) => res.render('404.ejs', { path: '/404' }));
 
 module.exports = router;

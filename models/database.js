2f3651efe585c81e844dbc677861728bc558dc56
joshuasearle <joshryansearle@gmail.com>
2020-09-04 16:23:57 +1000

    Add database file

---
+++ models/database.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Book = require('./book-model');
+const Author = require('./author-model');
+
+const findBooks = () => Book.find();
+
+const addBook = async (title, author, isbn, created, summary) => {
+  // Get authors with same name
+  const authors = await Author.where({
+    'name.firstName': author.first,
+    'name.lastName': author.last,
+  });
+
+  if (authors.length === 0) throw new Error('Author does not exist.');
+
+  // Create book fields
+  const bookFields = {
+    _id: new mongoose.Types.ObjectId(),
+    title: title,
+    authorId: authors[0]._id,
+  };
+  if (isbn) bookFields.isbn = isbn;
+  if (created) bookFields.created = created;
+  if (summary) bookFields.summary = created;
+
+  // Create book and save
+  const newBook = new Book(bookFields);
+  const saveResult = await newBook.save();
+  return saveResult;
+};
+
+module.exports = { findBooks, addBook };

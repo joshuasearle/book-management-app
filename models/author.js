a7d38b1a3c9a6469934b0b276e28517f4e5de484
joshuasearle <joshryansearle@gmail.com>
2020-09-04 14:16:01 +1000

    Create author schema

---
+++ models/author.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const authorSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  name: {
+    firstName: {
+      type: String,
+      required: true,
+      validate: {
+        validator: firstName => firstName.length > 0,
+        message: 'First name cannot be empty',
+      },
+    },
+    lastName: {
+      type: String,
+    },
+  },
+  dob: Date,
+  address: {
+    state: {
+      type: String,
+      validate: {
+        validator: state => state.length === 2 || state.length === 3,
+        message: 'State must be 2 or 3 characters.',
+      },
+    },
+    suburb: String,
+    Street: String,
+    Unit: Number,
+  },
+  numBooks: {
+    type: Number,
+    validate: {
+      validator: numBooks => numBooks > 0,
+      message: 'Author needs at least one book.',
+    },
+  },
+});

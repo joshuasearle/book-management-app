b3383430c3662761b45bb2260af365050be67406
joshuasearle <joshryansearle@gmail.com>
2020-09-10 19:58:05 +1000

    Fix date conversion bug

--- util/util.js
+++ util/util.js
@@ -1,8 +1,8 @@
 const convertDate = date => {
-  const d = new Date(date),
-    month = '' + (d.getMonth() + 1),
-    day = '' + d.getDate(),
-    year = d.getFullYear();
+  let d = new Date(date);
+  let month = '' + (d.getMonth() + 1);
+  let day = '' + d.getDate();
+  let year = d.getFullYear();
 
   if (month.length < 2) month = '0' + month;
   if (day.length < 2) day = '0' + day;
